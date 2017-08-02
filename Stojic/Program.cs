8371fab52c96694fa307aa631ea82e1a6340ab49
seb <sebastien.dubos@gmail.com>
2017-08-02 21:37:25 +0200

    Adapted Mac

--- Stojic/Program.cs
+++ Stojic/Program.cs
@@ -13,6 +13,7 @@ namespace Stojic
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseUrls("http://*:5111/Stojic-Matic")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
